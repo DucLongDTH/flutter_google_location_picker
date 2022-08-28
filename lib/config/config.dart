98144e59c0119cc740e0481154204807d025c3b6
Abdul Rehman <arsarsars1@gmail.com>
2022-08-28 21:37:00 +0500

    * Template fix

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,4 +1,6 @@
 class Config {
   static const String url =
       "https://nominatim.openstreetmap.org/reverse?format=json";
+  static const String urlTemplate =
+      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 }
